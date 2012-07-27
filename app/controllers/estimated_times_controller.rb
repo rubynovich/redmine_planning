02da914729fe409256899865521fb9f56031bf03
Rubynovich <roman@shipiev.me>
2012-07-27 13:07:34 +0400

    maket

--- app/controllers/estimated_times_controller.rb
+++ app/controllers/estimated_times_controller.rb
@@ -1,6 +1,23 @@
 class EstimatedTimesController < ApplicationController
   unloadable
-
+  
+  helper :timelog
+  include TimelogHelper
+  
   def index
+    @current_date = if params[:current_date].blank? 
+      Date.today
+    else
+      Date.parse(params[:current_date])
+    end
+    @current_date -= @current_date.wday.days - 1.day
+    @current_user = User.current
+    @current_dates = [@current_date-2.week, @current_date-1.week, @current_date, @current_date+1.week, @current_date+2.week]
+    @assigned_issues = Issue.visible.open.find(:all, 
+      :conditions => {
+        :assigned_to_id => ([User.current.id] + User.current.group_ids)}, 
+      :limit => 10, 
+      :include => [ :status, :project, :tracker, :priority ], 
+      :order => "#{IssuePriority.table_name}.position DESC, #{Issue.table_name}.updated_on DESC")
   end
 end
