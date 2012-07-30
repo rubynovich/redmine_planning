a67840b8113a1d506c7c62d05d6663cf0d6a3bff
Rubynovich <roman@shipiev.me>
2012-07-30 17:42:54 +0400

    add new and validations

--- app/controllers/estimated_times_controller.rb
+++ app/controllers/estimated_times_controller.rb
@@ -1,36 +1,48 @@
 class EstimatedTimesController < ApplicationController
   unloadable
   
+  before_filter :add_info, :only => [:new, :index]
+  
   helper :timelog
   include TimelogHelper
   
   def index
-    @current_date = if params[:current_date].blank? 
-      Date.today
-    else
-      Date.parse(params[:current_date])
-    end
-    @current_date -= @current_date.wday.days - 1.day
-    @current_user = User.current
-    @current_dates = [@current_date-2.week, @current_date-1.week, @current_date, @current_date+1.week, @current_date+2.week]
-    @assigned_issues = Issue.visible.open.find(:all, 
-      :conditions => {
-        :assigned_to_id => ([User.current.id] + User.current.group_ids)}, 
-      :limit => 10, 
-      :include => [ :status, :project, :tracker, :priority ], 
-      :order => "#{IssuePriority.table_name}.position DESC, #{Issue.table_name}.due_date")
+    @estimated_time = EstimatedTime.new
+  end
+
+  def new
+    @estimated_time = EstimatedTime.new(params[:estimated_time])
   end
   
   def create
-      project_id = Issue.find(params[:estimated_time][:issue_id]).project.id
-      plan_on = Date.parse(params[:estimated_time][:plan_on])
-      tyear = plan_on.year
-      tmonth = plan_on.month
-      tweek = plan_on.cweek
-      flash[:notice] = l(:notice_successful_create)      
-      redirect_to :action => :index, :current_date => params[:current_date]
-#      render :action => :index, :current_date => params[:current_date]
+      @estimated_time = EstimatedTime.new(params[:estimated_time])  
+      if @estimated_time.present? && @estimated_time.save
+        flash[:notice] = l(:notice_successful_create)      
+        redirect_to :action => :index, :current_date => params[:current_date]
+      else
+        add_info
+        render :action => :new, :current_date => params[:current_date]
+      end
 #    rescue
 #      render_404      
   end
+  
+  private
+  
+    def add_info
+      @current_date = if params[:current_date].blank? 
+        Date.today
+      else
+        Date.parse(params[:current_date])
+      end
+      @current_date -= @current_date.wday.days - 1.day
+      @current_user = User.current
+      @current_dates = [@current_date-2.week, @current_date-1.week, @current_date, @current_date+1.week, @current_date+2.week]
+      @assigned_issues = Issue.visible.open.find(:all, 
+        :conditions => {
+          :assigned_to_id => ([User.current.id] + User.current.group_ids)}, 
+        :limit => 10, 
+        :include => [ :status, :project, :tracker, :priority ], 
+        :order => "#{IssuePriority.table_name}.position DESC, #{Issue.table_name}.due_date")      
+    end
 end
--- app/models/estimated_time.rb
+++ app/models/estimated_time.rb
@@ -1,3 +1,23 @@
 class EstimatedTime < ActiveRecord::Base
   unloadable
+  
+  belongs_to :user
+  belongs_to :project
+  belongs_to :issue
+  
+  before_save :add_info
+  
+  validates_presence_of :issue_id, :hours, :plan_on
+  validates_numericality_of :hours
+  validates_uniqueness_of :issue_id, :scope => :user_id
+  
+  def add_info
+    if self.valid?
+      self.tyear = plan_on.year
+      self.tmonth = plan_on.month
+      self.tweek = plan_on.cweek
+      self.user_id = User.current.id
+      self.project_id = Issue.find(self.issue_id).project.id
+    end
+  end
 end
