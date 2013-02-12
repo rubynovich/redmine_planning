4d4e013bd830755c08dbb4e3f4b6f6b2ef246dee
rubynovich <roman@shipiev.me>
2013-02-12 12:56:45 +0400

    translate init-description from russian to bad english

--- init.rb
+++ init.rb
@@ -1,4 +1,25 @@
 require 'redmine'
+
+Redmine::Plugin.register :redmine_planning do
+  name 'Planning'
+  author 'Roman Shipiev'
+  description 'The plugin allows schedule time on issues'
+  version '0.0.6'
+  url 'https://github.com/rubynovich/redmine_planning'
+  author_url 'http://roman.shipiev.me'
+
+  project_module :planning do
+    permission :view_planning, :estimated_times => [:index], :public => true
+  end
+
+  menu :top_menu, :estimated_times, {:controller => :estimated_times, :action => :index}, :caption => :label_planning, :param => :project_id, :if => Proc.new{User.current.is_planning_manager?}
+
+  menu :project_menu, :estimated_times, {:controller => :estimated_times, :action => :index}, :caption => :label_planning, :param => :project_id, :if => Proc.new{User.current.is_planning_manager?}, :require => :member
+
+  menu :admin_menu, :planning_manager,
+    {:controller => :planning_managers, :action => :index}, :caption => :label_planning_manager_plural, :html => {:class => :users}
+end
+
 if Rails::VERSION::MAJOR < 3
   require 'dispatcher'
   object_to_prepare = Dispatcher
@@ -11,30 +32,9 @@ object_to_prepare.to_prepare do
     require "planning_#{cl}_patch"
   end
 
-  [ [Issue, PlanningPlugin::IssuePatch], [User, PlanningPlugin::UserPatch], 
+  [ [Issue, PlanningPlugin::IssuePatch], [User, PlanningPlugin::UserPatch],
     [TimeEntry, PlanningPlugin::TimeEntryPatch], [TimelogHelper, PlanningPlugin::TimelogHelperPatch]
   ].each do |cl, patch|
     cl.send(:include, patch) unless cl.included_modules.include? patch
   end
 end
-
-Redmine::Plugin.register :redmine_planning do
-  name 'Планирование'
-  author 'Roman Shipiev'
-  description 'Позволяет планировать время на ту или иную задачу. Планировать можно вплоть до срока выполнения задачи.'
-  version '0.0.6'
-  url 'https://github.com/rubynovich/redmine_planning'
-  author_url 'http://roman.shipiev.me'
-  
-  project_module :planning do  
-    permission :view_planning, :estimated_times => [:index], :public => true
-  end
-      
-  menu :top_menu, :estimated_times, {:controller => :estimated_times, :action => :index}, :caption => :label_planning, :param => :project_id, :if => Proc.new{User.current.is_planning_manager?}
-  
-  menu :project_menu, :estimated_times, {:controller => :estimated_times, :action => :index}, :caption => :label_planning, :param => :project_id, :if => Proc.new{User.current.is_planning_manager?}, :require => :member
-  
-  menu :admin_menu, :planning_manager, 
-    {:controller => :planning_managers, :action => :index}, :caption => :label_planning_manager_plural, :html => {:class => :users}
-end
-
