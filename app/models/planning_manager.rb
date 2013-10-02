c8c56d338f1f274386a684f574c3aa0247cd365a
rubynovich <roman@shipiev.me>
2013-10-02 10:24:39 +0400

    add: subordinates.sorted

--- app/models/planning_manager.rb
+++ app/models/planning_manager.rb
@@ -1,15 +1,15 @@
 class PlanningManager < ActiveRecord::Base
   unloadable
-  
+
   belongs_to :user
 
   has_many :subordinates
 
-  validates_presence_of :user_id  
+  validates_presence_of :user_id
   validates_uniqueness_of :user_id
 
   def active_subordinates
-    User.where(:id => self.subordinates.pluck(:principal_id), :status => User::STATUS_ACTIVE) #.sort_by(&:name)
+    User.where(id: self.subordinates.pluck(:principal_id)).sorted.active
   end
-  
+
 end
