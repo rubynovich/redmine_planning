6a8c06577904e30dd934077230ca9d7fd347c33e
Maksim <max@koritskiy.ru>
2014-07-30 10:53:24 +0400

    fix confirmation_type method

--- app/controllers/planning_confirmations_controller.rb
+++ app/controllers/planning_confirmations_controller.rb
@@ -6,7 +6,7 @@ class PlanningConfirmationsController < ApplicationController
   	confirmation_type = params[:type] == "0" ? :KGIP_confirmation : :head_confirmation
   	confirms = PlanningConfirmation.where(id: params[:id]).where(["date_start <= ? ", Date.today-1.week])
   	if confirm = confirms.first
-	  	column_value = confirm.confirmation_type
+	  	column_value = confirmation_type
 	  	confirm.update_column(confirmation_type, !column_value)
       if (! confirm.KGIP_confirmation) && (! confirm.head_confirmation)
         confirm.update_column(:hours, 0.0)
