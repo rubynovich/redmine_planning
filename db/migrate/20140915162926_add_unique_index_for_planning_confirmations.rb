1646abc08aabde27960f9a0751c1c940ab6df097
Maksim <max@koritskiy.ru>
2014-09-15 20:37:13 +0400

    fix index name

--- db/migrate/20140915162926_add_unique_index_for_planning_confirmations.rb
+++ db/migrate/20140915162926_add_unique_index_for_planning_confirmations.rb
@@ -1,9 +1,9 @@
 class AddUniqueIndexForPlanningConfirmations < ActiveRecord::Migration
   def up
-    add_index :planning_confirmations, [:user_id, :issue_id, :date_start], unique: true
+    add_index :planning_confirmations, [:user_id, :issue_id, :date_start], unique: true, name: 'index_planning_confirmations_unique'
   end
 
   def down
-    remove_index :planning_confirmations, [:user_id, :issue_id, :date_start]
+    remove_index :planning_confirmations, :name => 'index_planning_confirmations_unique'
   end
 end
